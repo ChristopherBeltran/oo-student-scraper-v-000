ff89676450c2a559df8c1491cfd18a9d1d6ef7a6
Christopher Beltran <christod.beltran@gmail.com>
2018-09-12 02:56:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    self.send(student_hash)
-    
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
     @@all << self 
   end
 
